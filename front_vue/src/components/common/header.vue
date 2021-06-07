61b0425783139107d004ccf4cb333e5e2d5aed17
whitejh <jhk0806@gmail.com>
2021-06-07 16:46:46 +0900

    Vue2 -> Vue3 변경

--- front_vue/src/components/common/header.vue
+++
@@ -1,7 +0,0 @@
-<template>
- <div class="header">
-    <router-link to="/trade">댕댕거래</router-link> |
-    <router-link to="/feed">댕댕피드</router-link> | 
-    <router-link to="/notice">댕댕알림</router-link>
- </div>
-</template>
