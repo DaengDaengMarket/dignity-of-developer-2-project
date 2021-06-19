4a345b6ffe0a604e1300b27b7bb14ea759680cc8
hackjap <hackjap1015@gmail.com>
2021-06-19 18:57:09 +0900

    veautify

--- front_vue/src/views/Home.vue
+++ front_vue/src/views/Home.vue
@@ -1,5 +1,9 @@
 <template>
+
   <div id="mypage">
+
+
+
     <!-- 배너슬라이드  -->
     <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
       <div class="carousel-indicators">
@@ -47,19 +51,13 @@
         <span class="visually-hidden">Next</span>
       </button>
     </div>
-    
+
     <!-- Off-Canvas Wrapper-->
     <div class="offcanvas-wrapper">
       <div class="container padding-bottom-1x mb-1">
-        <div
-          class="tab-content"
-          id="myTabContent">
+        <div class="tab-content" id="myTabContent">
           <!--  탭 1 : 거래목록  -->
-          <div
-            class="tab-pane fade show active"
-            id="itemlist"
-            role="tabpanel"
-            aria-labelledby="itemlist-tab">
+          <div class="tab-pane fade show active" id="itemlist" role="tabpanel" aria-labelledby="itemlist-tab">
             <!-- 상품 리스트 메뉴 -->
             <div class="shop-toolbar padding-top-2x">
               <h2 class="text-center mb-30">
@@ -71,9 +69,7 @@
             <div class="card-group">
               <!-- 카드 1 -->
               <div class="card">
-                <img
-                  src="../../public/img/shop/products/01.jpg"
-                  class="card-img-top"
+                <img src="../../public/img/shop/products/01.jpg" class="card-img-top"
                   alt="../../public/img/shop/products/01.jpg" />
                 <div class="card-body">
                   <h5 class="card-title">
@@ -88,9 +84,7 @@
 
                   <router-link to="/productdetail">
                     <div class="card-button">
-                      <button
-                        type="button"
-                        class="btn btn-primary btn-sm">
+                      <button type="button" class="btn btn-primary btn-sm">
                         상세 정보 보기
                       </button>
                     </div>
@@ -102,9 +96,7 @@
               </div>
               <!-- 카드 2 -->
               <div class="card">
-                <img
-                  src="../../public/img/shop/products/02.jpg"
-                  class="card-img-top"
+                <img src="../../public/img/shop/products/02.jpg" class="card-img-top"
                   alt="../../public/img/shop/products/01.jpg" />
                 <div class="card-body">
                   <h5 class="card-title">
@@ -117,9 +109,7 @@
                     삼성2동
                   </p>
                   <div class="card-button">
-                    <button
-                      type="button"
-                      class="btn btn-primary btn-sm">
+                    <button type="button" class="btn btn-primary btn-sm">
                       상세 정보 보기
                     </button>
                   </div>
@@ -130,9 +120,7 @@
               </div>
               <!-- 카드 3 -->
               <div class="card">
-                <img
-                  src="../../public/img/shop/products/03.jpg"
-                  class="card-img-top"
+                <img src="../../public/img/shop/products/03.jpg" class="card-img-top"
                   alt="../../public/img/shop/products/01.jpg" />
                 <div class="card-body">
                   <h5 class="card-title">
@@ -145,9 +133,7 @@
                     압구정동
                   </p>
                   <div class="card-button">
-                    <button
-                      type="button"
-                      class="btn btn-primary btn-sm">
+                    <button type="button" class="btn btn-primary btn-sm">
                       상세 정보 보기
                     </button>
                   </div>
@@ -159,9 +145,7 @@
 
               <!-- 카드 4 -->
               <div class="card">
-                <img
-                  src="../../public/img/shop/products/04.jpg"
-                  class="card-img-top"
+                <img src="../../public/img/shop/products/04.jpg" class="card-img-top"
                   alt="../../public/img/shop/products/04.jpg" />
                 <div class="card-body">
                   <h5 class="card-title">
@@ -174,9 +158,7 @@
                     신사동
                   </p>
                   <div class="card-button">
-                    <button
-                      type="button"
-                      class="btn btn-primary btn-sm">
+                    <button type="button" class="btn btn-primary btn-sm">
                       상세 정보 보기
                     </button>
                   </div>
@@ -191,9 +173,7 @@
             <div class="card-group">
               <!-- 카드 1 -->
               <div class="card">
-                <img
-                  src="../../public/img/shop/products/05.jpg"
-                  class="card-img-top"
+                <img src="../../public/img/shop/products/05.jpg" class="card-img-top"
                   alt="../../public/img/shop/products/01.jpg" />
                 <div class="card-body">
                   <h5 class="card-title">
@@ -206,9 +186,7 @@
                     대치동
                   </p>
                   <div class="card-button">
-                    <button
-                      type="button"
-                      class="btn btn-primary btn-sm">
+                    <button type="button" class="btn btn-primary btn-sm">
                       상세 정보 보기
                     </button>
                   </div>
@@ -219,9 +197,7 @@
               </div>
               <!-- 카드 2 -->
               <div class="card">
-                <img
-                  src="../../public/img/shop/products/06.jpg"
-                  class="card-img-top"
+                <img src="../../public/img/shop/products/06.jpg" class="card-img-top"
                   alt="../../public/img/shop/products/01.jpg" />
                 <div class="card-body">
                   <h5 class="card-title">
@@ -234,9 +210,7 @@
                     도곡1동
                   </p>
                   <div class="card-button">
-                    <button
-                      type="button"
-                      class="btn btn-primary btn-sm">
+                    <button type="button" class="btn btn-primary btn-sm">
                       상세 정보 보기
                     </button>
                   </div>
@@ -247,9 +221,7 @@
               </div>
               <!-- 카드 3 -->
               <div class="card">
-                <img
-                  src="../../public/img/shop/products/07.jpg"
-                  class="card-img-top"
+                <img src="../../public/img/shop/products/07.jpg" class="card-img-top"
                   alt="../../public/img/shop/products/01.jpg" />
                 <div class="card-body">
                   <h5 class="card-title">
@@ -262,9 +234,7 @@
                     역삼1동
                   </p>
                   <div class="card-button">
-                    <button
-                      type="button"
-                      class="btn btn-primary btn-sm">
+                    <button type="button" class="btn btn-primary btn-sm">
                       상세 정보 보기
                     </button>
                   </div>
@@ -276,9 +246,7 @@
 
               <!-- 카드 4 -->
               <div class="card">
-                <img
-                  src="../../public/img/shop/products/08.jpg"
-                  class="card-img-top"
+                <img src="../../public/img/shop/products/08.jpg" class="card-img-top"
                   alt="../../public/img/shop/products/04.jpg" />
                 <div class="card-body">
                   <h5 class="card-title">
@@ -291,9 +259,7 @@
                     역삼2동
                   </p>
                   <div class="card-button">
-                    <button
-                      type="button"
-                      class="btn btn-primary btn-sm">
+                    <button type="button" class="btn btn-primary btn-sm">
                       상세 정보 보기
                     </button>
                   </div>
@@ -308,9 +274,7 @@
             <div class="card-group">
               <!-- 카드 1 -->
               <div class="card">
-                <img
-                  src="../../public/img/shop/products/09.jpg"
-                  class="card-img-top"
+                <img src="../../public/img/shop/products/09.jpg" class="card-img-top"
                   alt="../../public/img/shop/products/09.jpg" />
                 <div class="card-body">
                   <h5 class="card-title">
@@ -323,9 +287,7 @@
                     성수동
                   </p>
                   <div class="card-button">
-                    <button
-                      type="button"
-                      class="btn btn-primary btn-sm">
+                    <button type="button" class="btn btn-primary btn-sm">
                       상세 정보 보기
                     </button>
                   </div>
@@ -336,9 +298,7 @@
               </div>
               <!-- 카드 2 -->
               <div class="card">
-                <img
-                  src="../../public/img/shop/products/10.jpg"
-                  class="card-img-top"
+                <img src="../../public/img/shop/products/10.jpg" class="card-img-top"
                   alt="../../public/img/shop/products/10.jpg" />
                 <div class="card-body">
                   <h5 class="card-title">
@@ -351,9 +311,7 @@
                     성수동
                   </p>
                   <div class="card-button">
-                    <button
-                      type="button"
-                      class="btn btn-primary btn-sm">
+                    <button type="button" class="btn btn-primary btn-sm">
                       상세 정보 보기
                     </button>
                   </div>
@@ -364,9 +322,7 @@
               </div>
               <!-- 카드 3 -->
               <div class="card">
-                <img
-                  src="../../public/img/shop/products/11.jpg"
-                  class="card-img-top"
+                <img src="../../public/img/shop/products/11.jpg" class="card-img-top"
                   alt="../../public/img/shop/products/11.jpg" />
                 <div class="card-body">
                   <h5 class="card-title">
@@ -379,9 +335,7 @@
                     성수동
                   </p>
                   <div class="card-button">
-                    <button
-                      type="button"
-                      class="btn btn-primary btn-sm">
+                    <button type="button" class="btn btn-primary btn-sm">
                       상세 정보 보기
                     </button>
                   </div>
@@ -393,9 +347,7 @@
 
               <!-- 카드 4 -->
               <div class="card">
-                <img
-                  src="../../public/img/shop/products/12.jpg"
-                  class="card-img-top"
+                <img src="../../public/img/shop/products/12.jpg" class="card-img-top"
                   alt="../../public/img/shop/products/12.jpg" />
                 <div class="card-body">
                   <h5 class="card-title">
@@ -408,9 +360,7 @@
                     성수동
                   </p>
                   <div class="card-button">
-                    <button
-                      type="button"
-                      class="btn btn-primary btn-sm">
+                    <button type="button" class="btn btn-primary btn-sm">
                       상세 정보 보기
                     </button>
                   </div>
@@ -445,82 +395,64 @@
 
           <!-- 모달용 버튼 -->
           <div class="modal-test">
-            <button
-              class="btn btn-outline-primary"
-              type="button"
-              data-bs-toggle="modal"
-              data-bs-target="#review-modal">
+            <button class="btn btn-outline-primary" type="button" data-bs-toggle="modal" data-bs-target="#review-modal">
               상품 후기 등록
             </button>
           </div>
         </div>
       </div>
     </div>
-  </div>
 
+    <!-- Back To Top Button-->
+    <a class="scroll-to-top-btn" href="#"><i class="icon-arrow-up"></i></a>
+    <!-- Backdrop-->
+    <div class="site-backdrop"></div>
 
-  <!-- Back To Top Button-->
-  <a
-    class="scroll-to-top-btn"
-    href="#"><i class="icon-arrow-up"></i></a>
-  <!-- Backdrop-->
-  <div class="site-backdrop"></div>
-
-  <!-- 상품후기 모달 -->
-  <div
-    class="modal fade"
-    id="review-modal"
-    tabindex="-1"
-    role="dialog">
-    <div
-      class="modal-dialog modal-dialog-centered"
-      role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h3 class="modal-title">
-            거래 후기
-          </h3>
-          <button
-            class="close"
-            type="button"
-            data-bs-dismiss="modal"
-            aria-label="Close">
-            <span
-              aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <p>댕댕 정보: 금 목줄</p>
-          <p style="display: inline-block;">
-            평가:
-          </p><div class="rating-stars">
-            <i class="icon-star filled"></i>
-            <i class="icon-star filled"></i>
-            <i class="icon-star filled"></i>
-            <i class="icon-star filled"></i>
-            <i class="icon-star"></i>
+    <!-- 상품후기 모달 -->
+    <div class="modal fade" id="review-modal" tabindex="-1" role="dialog">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h3 class="modal-title">
+              거래 후기
+            </h3>
+            <button class="close" type="button" data-bs-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
           </div>
-        
-          <div class="form-group row">
-            <div class="col-10">
-              <textarea
-                class="form-control"
-                id="textarea-input"
-                rows="5"
-                placeholder="상세 내용을 작성해주세요."></textarea>
+          <div class="modal-body">
+            <p>댕댕 정보: 금 목줄</p>
+            <p style="display: inline-block;">
+              평가:
+            </p>
+            <div class="rating-stars">
+              <i class="icon-star filled"></i>
+              <i class="icon-star filled"></i>
+              <i class="icon-star filled"></i>
+              <i class="icon-star filled"></i>
+              <i class="icon-star"></i>
+            </div>
+
+            <div class="form-group row">
+              <div class="col-10">
+                <textarea class="form-control" id="textarea-input" rows="5" placeholder="상세 내용을 작성해주세요."></textarea>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="modal-footer">
-          <button
-            class="btn btn-primary btn-sm"
-            type="button">
-            후기 작성 완료
-          </button>
+          <div class="modal-footer">
+            <button class="btn btn-primary btn-sm" type="button">
+              후기 작성 완료
+            </button>
+          </div>
         </div>
       </div>
     </div>
+
   </div>
+
+
+
+
 </template>
 
 <script>
@@ -530,25 +462,29 @@
   .card {
     padding: 20px;
   }
+
   .card-group {
     margin-bottom: 3%;
   }
+
   .card-body>*,
   .card-footer {
     display: flex;
     justify-content: center;
   }
+
   .dropdown>button {
     height: 35px;
     display: flex;
     align-items: center;
-    
+
   }
+
   .card-button>button {
     margin-left: 10px;
   }
- 
-  .comment{
-        width: 90%;
+
+  .comment {
+    width: 90%;
   }
 </style>
