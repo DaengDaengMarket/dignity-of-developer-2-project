113897cf88077e1528022640cdba2ea38720d502
Gyu <melon9751@naver.com>
2021-06-21 12:38:25 +0900

    [feat] 상품 컨트롤러 추가

---
+++ BE/src/main/java/com/dignity/puppymarket/controller/ItemController.java
@@ -0,0 +1,22 @@
+package com.dignity.puppymarket.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/item")
+public class ItemController {
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping("")
+    public List<ItemResponseDto> list() {
+        return itemService.getItems();
+    }
+}
