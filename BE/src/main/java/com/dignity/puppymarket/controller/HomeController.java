6938061244f03b31d3eb0eeef19b35fea7179710
Gyu <melon9751@naver.com>
2021-06-21 16:39:36 +0900

    [feat] 홈화면 조회 컨트롤러 추가

--- BE/src/main/java/com/dignity/puppymarket/controller/HomeController.java
+++ BE/src/main/java/com/dignity/puppymarket/controller/HomeController.java
@@ -1,12 +1,29 @@
 package com.dignity.puppymarket.controller;
 
+import com.dignity.puppymarket.dto.AdvertiseResponseDto;
+import com.dignity.puppymarket.dto.HomeResponseDto;
+import com.dignity.puppymarket.dto.Item.ItemHomeGetResponseDto;
+import com.dignity.puppymarket.service.AdvertiseService;
+import com.dignity.puppymarket.service.ItemService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class HomeController {
+    private final ItemService itemService;
+    private final AdvertiseService advertiseService;
+
+    public HomeController(ItemService itemService, AdvertiseService advertiseService) {
+        this.itemService = itemService;
+        this.advertiseService = advertiseService;
+    }
+
     @GetMapping("/")
-    public String Home() {
-        return "index";
+    public HomeResponseDto Home() {
+        List<ItemHomeGetResponseDto> itemList = itemService.getHomeItems();
+        List<AdvertiseResponseDto> advertiseList = advertiseService.getAdvertises();
+        return HomeResponseDto.of(itemList, advertiseList);
     }
 }
