760f777b02d321a6a4720e3569b78b9f46f66c8c
Gyu <melon9751@naver.com>
2021-06-21 15:29:06 +0900

    [feat] 홈화면 조회 dto 추가

---
+++ BE/src/main/java/com/dignity/puppymarket/dto/HomeResponseDto.java
@@ -0,0 +1,22 @@
+package com.dignity.puppymarket.dto;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class HomeResponseDto {
+    private List<ItemHomeGetResponseDto> itemList;
+
+    private List<AdvertiseResponseDto> advertiseList;
+
+    @Builder
+    public HomeResponseDto(List<ItemHomeGetResponseDto> itemList, List<AdvertiseResponseDto> advertiseList) {
+        this.itemList = itemList;
+        this.advertiseList = advertiseList;
+    }
+}
