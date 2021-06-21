be09b33582c41f2fcbed03b4c70c85977c9824d5
Gyu <melon9751@naver.com>
2021-06-21 16:22:05 +0900

    [feat] 상품 홈화면 조회를 위한 dto 변환 메서드  추가

--- BE/src/main/java/com/dignity/puppymarket/dto/Item/ItemHomeGetResponseDto.java
+++ BE/src/main/java/com/dignity/puppymarket/dto/Item/ItemHomeGetResponseDto.java
@@ -1,9 +1,11 @@
 package com.dignity.puppymarket.dto.Item;
 
 import com.dignity.puppymarket.domain.Gu;
+import com.dignity.puppymarket.domain.Item;
 import com.dignity.puppymarket.domain.ItemImage;
 import com.dignity.puppymarket.domain.Si;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -26,4 +28,28 @@ public class ItemHomeGetResponseDto {
     private Si si;
 
     private Gu gu;
+
+    @Builder
+    public ItemHomeGetResponseDto(Long id, String name, int price, LocalDateTime createdAt,
+                                  List<ItemImage> itemImageList, Si si, Gu gu) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.createdAt = createdAt;
+        this.itemImageList = itemImageList;
+        this.si = si;
+        this.gu = gu;
+    }
+
+    public static ItemHomeGetResponseDto of(Item item) {
+        return ItemHomeGetResponseDto.builder()
+                .id(item.getId())
+                .name(item.getName())
+                .price(item.getPrice())
+                .createdAt(item.getCreatedAt())
+                .itemImageList(item.getItemImageList())
+                .si(item.getSi())
+                .gu(item.getGu())
+                .build();
+    }
 }
