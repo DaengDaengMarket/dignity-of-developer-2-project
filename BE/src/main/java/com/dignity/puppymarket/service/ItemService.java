066c23c6cb60d8d016c728942aef9da723c812e8
Gyu <melon9751@naver.com>
2021-06-21 16:16:24 +0900

    [feat] 상품 서비스에 홈 전용 조회 추가

--- BE/src/main/java/com/dignity/puppymarket/service/ItemService.java
+++ BE/src/main/java/com/dignity/puppymarket/service/ItemService.java
@@ -1,5 +1,6 @@
 package com.dignity.puppymarket.service;
 
+import com.dignity.puppymarket.dto.Item.ItemHomeGetResponseDto;
 import com.dignity.puppymarket.dto.Item.ItemResponseDto;
 import com.dignity.puppymarket.repository.ItemRepository;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,12 @@ public class ItemService {
     public List<ItemResponseDto> getItems() {
         return itemRepository.findAll().stream()
                 .map(ItemResponseDto::of)
+                .collect(Collectors.toList());
+    }
+    
+    public List<ItemHomeGetResponseDto> getHomeItems() {
+        return itemRepository.findAll().stream()
+                .map(ItemHomeGetResponseDto::of)
                 .collect(Collectors.toList());
     }
 }
