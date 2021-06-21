ae2e46053268bf6888a8fb52b6de5e4bd971eb4d
Gyu <melon9751@naver.com>
2021-06-21 12:39:45 +0900

    [feat] 상품 서비스 추가

---
+++ BE/src/main/java/com/dignity/puppymarket/service/ItemService.java
@@ -0,0 +1,20 @@
+package com.dignity.puppymarket.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+    
+    public List<ItemResponseDto> getItems() {
+        return itemRepository.findAll().stream()
+                .map(ItemResponseDto::of)
+                .collect(Collectors.toList());
+    }
+}
