a2f83533355c24f29fcf8009ac865e1990d45d9b
Gyu <melon9751@naver.com>
2021-06-21 10:59:00 +0900

    [feat] 예외 공통 처리 컨트롤러 추가

---
+++ BE/src/main/java/com/dignity/puppymarket/controller/ControllerErrorAdvice.java
@@ -0,0 +1,16 @@
+package com.dignity.puppymarket.controller;
+
+import com.dignity.puppymarket.error.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerErrorAdvice {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(UserNotFoundException.class)
+    public ErrorResponse handleUserNotFound(UserNotFoundException ex) {
+        return ErrorResponse.of(ex.getMessage());
+    }
+}
